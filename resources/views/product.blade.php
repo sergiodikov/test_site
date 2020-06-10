c635f3d23d6d296001036550290687f902020950
sd <sergiodikov@users.noreply.github.com>
2020-06-10 23:54:40 +0300

    Кол-во товара

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,14 +3,14 @@
 @section('title', 'Товар')
 
 @section('content')
-        <h1>iPhone X 64GB</h1>
-        <h2>{{ $product }}</h2>
-        <p>Цена: <b>71990 ₽</b></p>
-        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
-        <p>Отличный продвинутый телефон с памятью на 64 gb</p>
-
-        <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
-
-            <input type="hidden" name="_token" value="rvNOPJlIzGJ7aKucg3e7WX6xLu4RxM0h5s4qijb6">        </form>
+    <h1>{{ $product->name }}</h1>
+    <h2>{{ $product->category->name }}</h2>
+    <p>Цена: <b>{{ $product->price }} руб.</b></p>
+    <img src="{{ Storage::url($product->image) }}">
+    <p>{{ $product->description }}</p>
+    @if($product->isAvailable())
+        <a class="btn btn-success" href="{{ route('basket-add', $product) }}">Добавить в корзину</a>
+    @else
+        Не доступен
+    @endif
 @endsection
