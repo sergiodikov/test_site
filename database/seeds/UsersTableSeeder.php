1f2f64575e6983acae3d744c818460db847132ab
sd <sergiodikov@users.noreply.github.com>
2020-06-18 21:39:15 +0300

    Структурированный Seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CategoriesTableSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -12,25 +12,11 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
-            [
-                'name' => 'Мобильные телефоны',
-                'code' => 'mobiles',
-                'description' => 'В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!',
-                'image' => 'categories/mobile.jpg',
-            ],
-            [
-                'name' => 'Портативная техника',
-                'code' => 'portable',
-                'description' => 'Раздел с портативной техникой.',
-                'image' => 'categories/portable.jpg',
-            ],
-            [
-                'name' => 'Бытовая техника',
-                'code' => 'appliances',
-                'description' => 'Раздел с бытовой техникой',
-                'image' => 'categories/appliance.jpg',
-            ],
+        DB::table('users')->insert([
+            'name' => 'Администратор',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin'),
+            'is_admin' => 1,
         ]);
     }
 }
