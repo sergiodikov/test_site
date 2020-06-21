11344d30d94c87a9785a1463c56920138b775114
a.dikov <a.dikov@petrovich.ru>
2020-06-21 11:42:10 +0300

    disabled reset action

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,10 +22,8 @@
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li @routeactive('index')><a href="{{ route('index') }}">@lang('main.all_products')</a></li>
-                <li @routeactive('categor*')><a href="{{ route('categories') }}">@lang('main.categories')</a>
-                </li>
+                <li @routeactive('categor*')><a href="{{ route('categories') }}">@lang('main.categories')</a></li>
                 <li @routeactive('basket*')><a href="{{ route('basket') }}">@lang('main.cart')</a></li>
-                <li><a href="{{ route('reset') }}">@lang('main.reset_project')</a></li>
                 <li><a href="{{ route('locale', __('main.set_lang')) }}">@lang('main.set_lang')</a></li>
 
                 <li class="dropdown">
